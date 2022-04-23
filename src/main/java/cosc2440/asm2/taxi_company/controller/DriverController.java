7ad0603f73ac9cdcb0a4fce8691420b80989ab1c
Quoc Bao Nguyen Luu <quocbaonguyenluu@Quocs-MacBook-Pro.local>
2022-04-23 09:19:17 +0700

    update function driver pick car

--- src/main/java/cosc2440/asm2/taxi_company/controller/DriverController.java
+++ src/main/java/cosc2440/asm2/taxi_company/controller/DriverController.java
@@ -1,5 +1,6 @@
 package cosc2440.asm2.taxi_company.controller;
 
+import cosc2440.asm2.taxi_company.model.Car;
 import cosc2440.asm2.taxi_company.model.Driver;
 import cosc2440.asm2.taxi_company.service.CarService;
 import cosc2440.asm2.taxi_company.service.DriverService;
@@ -47,16 +48,24 @@ public class DriverController {
     @RequestMapping(path = "/driver/pick", method = RequestMethod.GET)
     public String pickCarById(@RequestParam(value = "carVIN") Long carVIN,
                               @RequestParam(value = "driverId") Long driverID) {
-        if (carService.getCarById(carVIN) == null) {
+        Car carToUpdate = carService.getCarById(carVIN);
+        Driver driverToUpdate = driverService.getDriverById(driverID);
+
+        if (carToUpdate == null) {
             return String.format("Car with VIN %d does not exist!", carVIN);
         }
 
-        if (driverService.getDriverById(driverID) == null) {
+        if (driverToUpdate == null) {
             return String.format("Driver with id %d does not exist!", driverID);
         }
 
         // Handle more logic here (set car, set driver, set available,...)
+        driverToUpdate.setCar(carToUpdate);
+        carToUpdate.setDriver(driverToUpdate);
+        carToUpdate.setAvailable(false);
 
+        driverService.updateDriver(driverToUpdate);
+        carService.updateCar(carToUpdate);
 
         return String.format("Assign car with VIN %d to driver with id %d!", carVIN, driverID);
     }
