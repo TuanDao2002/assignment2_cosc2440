16032758c4498ee7135f6c5cbc7fa7540f9d791f
Bui Quang An <quangan186@gmail.com>
2022-04-30 14:52:47 +0700

    test getCustomerById

--- src/test/java/cosc2440/asm2/taxi_company/controller/CustomerControllerTest.java
+++ src/test/java/cosc2440/asm2/taxi_company/controller/CustomerControllerTest.java
@@ -1,17 +1,13 @@
 package cosc2440.asm2.taxi_company.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Iterables;
 import cosc2440.asm2.taxi_company.model.Customer;
 import cosc2440.asm2.taxi_company.repository.CustomerRepository;
 import cosc2440.asm2.taxi_company.service.CustomerService;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +20,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -54,27 +49,58 @@ class CustomerControllerTest {
     @Autowired
     protected MockMvc mockMvc;
 
-//    private List<Customer> setUpData(){
+    //    private List<Customer> setUpData(){
 //        return Arrays.asList(new Customer(1,"An Bui", "0123456789", "tphcm"),
 //                new Customer(2,"Tuan Dao", "9876543210", "tphcm"),
 //                new Customer(3,"Bao Nguyen", "0246802468", "tphcm"),
 //                new Customer(4,"Long Nguyen", "1357913579", "tphcm"));
 //    }
+    protected List<Customer> customers = new ArrayList<>();
+
+    @BeforeEach
+    void setUp(){
+        Customer c1 = new Customer(1L,"An Bui", "0123456789", "tphcm");
+        Customer c2 = new Customer(2L,"Tuan Dao", "9876543210", "tphcm");
+
+        customers.add(c1);
+        customers.add(c2);
+
+        Customer savedCustomer1 = customerRepository.save(c1);
+        Customer savedCustomer2 = customerRepository.save(c2);
+
+        Mockito.when(customerRepository.findAll()).thenReturn(customers);
+        Mockito.when(customerRepository.save(c1)).thenReturn(savedCustomer1);
+        Mockito.when(customerRepository.save(c2)).thenReturn(savedCustomer2);
+    }
 
     @Test
     void addCustomer() throws Exception {
         Customer customer = new Customer(1L,"Dao Kha Tuan", "22222", "tphcm");
         Mockito.when(customerRepository.save(customer)).thenReturn(customer);
-        assertEquals("Customer with id 1 added successfully!", customerService.addCustomer(customer));
+        assertEquals("Customer with id 1 added successfully!", customerController.addCustomer(customer));
 
         Customer customer2 = new Customer(2L,"new", "9999", "tphcm");
         Mockito.when(customerRepository.save(customer2)).thenReturn(customer2);
-        assertEquals("Customer with id 2 added successfully!", customerController.addCustomer(customer2));
+        assertEquals("Customer with id 2 added successfully!", customerService.addCustomer(customer2));
 
         mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
         MvcResult mvcResult =mockMvc.perform(MockMvcRequestBuilders.post("/customer").contentType(MediaType.APPLICATION_JSON_VALUE).content(objectMapper.writeValueAsString(customer))).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
-
         String stringResult = mvcResult.getResponse().getContentAsString();
         assertEquals("Customer with id 1 added successfully!", stringResult);
     }
+
+    @Test
+    void getCustomerById() throws Exception {
+        Long customerId = 1L;
+        Mockito.when(customerRepository.findById(customerId)).thenReturn(Optional.of(customers.get(0)));
+        Customer getCustomer = customerService.getCustomerById(customerId);
+        assertNotNull(getCustomer);
+        assertEquals(customerId, getCustomer.getId());
+        assertEquals(customers.get(0), getCustomer);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/customer" + "/" + getCustomer.getId()).contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
 }
