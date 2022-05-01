a543187248f042340b0957d1c5e7fb3733e3b36f
Quoc Bao Nguyen Luu <quocbaonguyenluu@Quocs-MacBook-Pro.local>
2022-05-01 19:03:49 +0700

    update driver controller test

--- src/test/java/cosc2440/asm2/taxi_company/controller/DriverControllerTest.java
+++ src/test/java/cosc2440/asm2/taxi_company/controller/DriverControllerTest.java
@@ -1,4 +1,85 @@
 package cosc2440.asm2.taxi_company.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cosc2440.asm2.taxi_company.model.Booking;
+import cosc2440.asm2.taxi_company.model.Driver;
+import cosc2440.asm2.taxi_company.repository.DriverRepository;
+import cosc2440.asm2.taxi_company.service.CarService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class DriverControllerTest {
+    @MockBean
+    private DriverRepository driverRepository;
+
+    @InjectMocks
+    @Autowired
+    private CarService carService;
+
+    @InjectMocks
+    @Autowired
+    private DriverController driverController;
+
+    @Autowired
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final List<Driver> driverList = setUpData();
+
+    private List<Driver> setUpData() {
+        Driver driver1 = new Driver(1L, "0321000x6", "0913981333", 4.9);
+        Driver driver2 = new Driver(2L, "4566106e2", "0919073118", 5.0);
+        Driver driver3 = new Driver(3L, "1451007x3", "0903102346", 4.2);
+
+        return List.of(driver1, driver2, driver3);
+    }
+
+    @BeforeEach
+    private void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(driverController).build();
+    }
+
+    @Test
+    public void getAllDrivers() throws Exception {
+        Mockito.when(driverRepository.findAll()).thenReturn(driverList);
+
+        ResponseEntity<List<Driver>> expectedResponse = new ResponseEntity<>(driverList, new HttpHeaders(), HttpStatus.OK);
+        ResponseEntity<List<Driver>> actualResponse = driverController.getAllDrivers(0, 20);
+        assertEquals(driverList.size(), actualResponse.getBody().size());
+        assertTrue(Objects.requireNonNull(expectedResponse.getBody()).containsAll(actualResponse.getBody()));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/driver")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+
 }
